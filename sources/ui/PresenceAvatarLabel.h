3ddc2cf8db915537eb366d552231efa6194ff1f2
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-17 12:02:02 +0300

    Preserve PresenceAvatarLabel license header

--- sources/ui/PresenceAvatarLabel.h
+++ sources/ui/PresenceAvatarLabel.h
@@ -2,6 +2,19 @@
  * Copyright 2026 Sergei Ilinykh
  *
  * This file is part of MatterLeast.
+ *
+ * MatterLeast is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MatterLeast is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with MatterLeast. if not, see https://www.gnu.org/licenses/.
  */
 
 #pragma once
