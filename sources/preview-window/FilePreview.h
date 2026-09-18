c9b1de50a1e379ef4efbaf387b3ee823a4779bed
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-18 20:55:47 +0300

    Track stable full-preview image geometry

--- sources/preview-window/FilePreview.h
+++ sources/preview-window/FilePreview.h
@@ -5,7 +5,7 @@
  *
  * Mattermost-QT is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * Mattermost-QT is distributed in the hope that it will be useful,
@@ -23,8 +23,9 @@
 #include <QDialog>
 #include <QImage>
 #include <QPixmap>
+#include <QSize>
 
-class QScrollArea;
+class QResizeEvent;
 
 namespace Ui {
 class FilePreview;
@@ -47,14 +48,19 @@ public:
                 const QString& fileAuthor,
                 QWidget* parent = nullptr);
     ~FilePreview();
+
+protected:
+    void resizeEvent(QResizeEvent* event) override;
+
 private:
-    QSize displaySizeForViewport(const QSize& viewportSize) const;
-    QSize initialViewportSize() const;
-    void updateImageGeometry(const QSize& viewportSize);
+    QSize fitImageSize(const QSize& availableSize) const;
+    QSize initialImageAreaSize() const;
+    QSize imageAreaForDialogSize(const QSize& dialogSize) const;
+    void updateDisplayedPixmap(const QSize& availableSize);
 
     Ui::FilePreview* ui;
-    QPixmap pixmap;
-    QScrollArea* scrollArea = nullptr;
+    QPixmap sourcePixmap;
+    QSize imageChromeSize;
 };
 
 } /* namespace Mattermost */
