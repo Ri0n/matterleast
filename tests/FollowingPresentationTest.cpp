5bea68fe7a2400b809d941f42a746919f3a12633
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-18 22:58:34 +0300

    Test compact Following row labels

---
+++ tests/FollowingPresentationTest.cpp
@@ -0,0 +1,46 @@
+#include <QtTest>
+
+#include "channel-tree/FollowingPresentation.h"
+
+using namespace Mattermost;
+
+class FollowingPresentationTest : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void threadLabelHasNoTypeGlyphPrefix()
+    {
+        QCOMPARE(followingThreadLabel(QStringLiteral("Town Square"),
+                                      QStringLiteral("Hello there")),
+                 QStringLiteral("Town Square \u2014 Hello there"));
+
+        const QString label = followingThreadLabel(
+            QStringLiteral("Town Square"),
+            QStringLiteral("Hello there"));
+        QVERIFY(!label.startsWith(QStringLiteral("@ ")));
+        QVERIFY(!label.startsWith(QStringLiteral("\u21aa ")));
+    }
+
+    void emptySnippetUsesOnlyChannelName()
+    {
+        QCOMPARE(followingThreadLabel(QStringLiteral("Town Square"),
+                                      QStringLiteral("   \n\t  ")),
+                 QStringLiteral("Town Square"));
+    }
+
+    void snippetRemainsCompact()
+    {
+        const QString message(FollowingThreadSnippetLength + 20, QLatin1Char('x'));
+        const QString label = followingThreadLabel(
+            QStringLiteral("Channel"),
+            message);
+
+        const QString snippet = label.section(QStringLiteral(" \u2014 "), 1);
+        QCOMPARE(snippet.size(), FollowingThreadSnippetLength);
+        QVERIFY(snippet.endsWith(QChar(0x2026)));
+    }
+};
+
+QTEST_MAIN(FollowingPresentationTest)
+#include "FollowingPresentationTest.moc"
